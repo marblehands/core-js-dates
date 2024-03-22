6a98564add76f4b32ca7609c2b49f022ea433fd5
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-22 23:04:00 +0100

    feat: complete isDateInPeriod task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -172,8 +172,11 @@ function getCountDaysOnPeriod(dateStart, dateEnd) {
  * '2024-02-02', { start: '2024-02-02', end: '2024-03-02' } => true
  * '2024-02-10', { start: '2024-02-02', end: '2024-03-02' } => true
  */
-function isDateInPeriod(/* date, period */) {
-  throw new Error('Not implemented');
+function isDateInPeriod(date, period) {
+  const targetDate = Date.parse(date);
+  const startDate = Date.parse(period.start);
+  const endDate = Date.parse(period.end);
+  return targetDate >= startDate && targetDate <= endDate;
 }
 
 /**
