571e24c5ceb1ccfc0207715b4404778b751ec4e4
Ania Chebysheva <anna.chebysheva@gmail.com>
2024-03-23 01:10:01 +0100

    feat: complete getNextFridayThe13th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -266,8 +266,17 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const year = date.getUTCFullYear();
+  let month = date.getUTCMonth();
+  let isFriday;
+  let newDate;
+  while (isFriday !== 4) {
+    month += 1;
+    newDate = new Date(year, month, 13);
+    isFriday = newDate.getUTCDay();
+  }
+  return newDate;
 }
 
 /**
